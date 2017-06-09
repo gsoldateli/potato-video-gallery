82b4f98c31d665cece3bf9122c0b5bc08bdec592
Guilherme Soldateli <guilherm3soldateli@gmail.com>
2017-06-09 12:45:51 +0000

    Added some interaction to buttons

--- js/main.js
+++ js/main.js
@@ -3,17 +3,26 @@
 var controlPanel = (function () {
 
 	var currentVideo;
+	var playerControls;
+
+	var buttons = {};
 
 	function init() {
-		currentVideo = document.getElementById('p-current-video');		
+		currentVideo = document.getElementById('p-current-video');	
+		playerControls = document.querySelector('.p-player__controls');	
+
+		_registerButtons();
+		buttons.volume.value = volume() * 100;
 	}
 
 	function play() {
 		currentVideo.play();
+		buttons.playPause.innerHTML = 'pause';
 	}
 
 	function pause() {
 		currentVideo.pause();
+		buttons.playPause.innerHTML = 'play_arrow';
 	}
 
 	function rewind() {
@@ -22,7 +31,7 @@ var controlPanel = (function () {
 
 	function volume(volume) {
 		
-		if(!volume)
+		if(volume === undefined )
 		{
 			return currentVideo.volume;
 		}
@@ -45,13 +54,57 @@ var controlPanel = (function () {
 		else if (currentVideo.webkitRequestFullscreen) {
 			currentVideo.webkitRequestFullscreen();
 		}		
-	}	
+	}
 
 	function _addCallback(callback, fn) {
 		currentVideo.addEventListener(callback, fn, false);
 	}
 
+	function _playPause() {
+		if( currentVideo.paused ) {
+			play();
+		}
+		else {
+			pause();
+		}
+	}
 
+	function _setVolume() {
+		var inputValue = buttons.volume.value;
+		console.log(inputValue);
+		if(parseInt(inputValue) === 0) {
+			buttons.volumeToggle.innerHTML = 'volume_off';
+		}
+		else if(inputValue < 50) {
+			buttons.volumeToggle.innerHTML = 'volume_down';
+		}
+		else {
+			buttons.volumeToggle.innerHTML = 'volume_up';
+		}
+
+		volume(inputValue / 100);
+	}
+
+	function _toggleSound() {
+		if(buttons.volume.value > 0 ) {
+			_setVolume(0);
+		} else {
+			_setVolume(1);
+		}
+	}
+
+	function _registerButtons() {
+		buttons.playPause = document.querySelector("#p-play-pause-button");
+		buttons.playPause.addEventListener('click',function(){ _playPause()} );
+		
+		buttons.volume = document.querySelector("#p-audio-range-input");
+		buttons.volume.addEventListener("mousemove", function(){ _setVolume(); });
+		
+		buttons.volumeToggle = document.querySelector("#p-audio-toggle-button");
+		buttons.volumeToggle.addEventListener('click',function(){ _toggleSound()} );
+		buttons.fullscreen = document.querySelector("#p-button-fullscreen");
+		buttons.fullscreen.addEventListener('click',function(){ setFullscreen()} );
+	}
 
 	return {
 		init: init,
@@ -60,7 +113,7 @@ var controlPanel = (function () {
 		rewind: rewind,
 		volume: volume,
 		ended: ended,
-		fullscreen: setFullscreen
+		setFullscreen: setFullscreen
 	};
 
 })();
